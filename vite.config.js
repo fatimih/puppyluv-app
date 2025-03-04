deb7aa5dd9b238b56647f735270ac0eff125994d
Fatimih Linz <fatimihlinz@Fatimihs-Laptop.local>
2025-03-04 14:25:14 -0800

    Updated Vite config to expose server

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allow external access
+    port: 3000         // Make sure it uses port 3000
+  }
 })
+
+
 
 
 
